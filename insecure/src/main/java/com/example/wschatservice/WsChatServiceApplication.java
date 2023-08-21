4f6c743f7cf7b70c018e9563ae5ef45d4fc08a25
thomaskrut <thomas.krut@yh.nackademin.se>
2023-08-21 21:06:14 +0200

    1st commit

---
+++ insecure/src/main/java/com/example/wschatservice/WsChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.wschatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsChatServiceApplication.class, args);
+	}
+
+}
