67f5cb8fbac514a9cac1e1af2228d8e90a6a6e5a
thomaskrut <thomas.krut@yh.nackademin.se>
2023-08-24 14:02:04 +0200

    DelegatingPasswordEncoder

--- secure/sql-injection/src/main/java/com/example/sql_injection/WebSecurityConfig.java
+++ secure/sql-injection/src/main/java/com/example/sql_injection/WebSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
--- secure/sql-injection/src/main/java/com/example/sql_injection/controller/PublicController.java
+++ secure/sql-injection/src/main/java/com/example/sql_injection/controller/PublicController.java
@@ -3,6 +3,7 @@ import com.example.sql_injection.model.User;
 import com.example.sql_injection.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +16,13 @@ import java.util.List;
 public class PublicController {
 
     @Autowired
-    BCryptPasswordEncoder bcrypt;
+    PasswordEncoder bcrypt;
 
     private final UserRepository pRepo;
 
     public PublicController(UserRepository pRepo) {
         this.pRepo = pRepo;
     }
-
 
     @PostMapping("/public/register")
     public String addUser(@RequestBody User user) {
